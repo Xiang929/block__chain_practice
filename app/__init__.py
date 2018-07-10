719c22ea5c8450ba925c08528b5d2a7d158953e3
Cubelrti <tankrity@gmail.com>
2018-07-10 21:58:01 +0800

    fix path 1

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,8 @@
 from flask import render_template, Flask
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path="/static")
 
 app.config.from_object('config')
-
 @app.route('/')
 def hello_world():
     return 'Hello World!'
