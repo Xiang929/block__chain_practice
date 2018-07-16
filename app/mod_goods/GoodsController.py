e295f23dad7af2c640acc60ef1ed2536d8d0cac6
Xiang Chaozhu <XiangCZ929@Gmail.com>
2018-07-16 22:40:35 +0800

    fix

--- app/mod_goods/GoodsController.py
+++ app/mod_goods/GoodsController.py
@@ -1,9 +1,11 @@
-from app import app
+import uuid
+
 from flask import render_template, request, g, redirect, url_for
+
+from app import MysqlService
+from app import app
 from app.mod_publisher.subscriber import Subscriber
 from config import *
-from app import MysqlService
-import uuid
 
 subscriber = Subscriber(SERVER, PORT)
 new_flag = False
@@ -12,7 +14,6 @@ new_flag = False
 @app.route('/searchGoods')
 def search_goods():
     if hasattr(g, 'userid'):
-        print(g.userid)
         return render_template('searchGoods.html')
     return redirect(url_for('login'))
 
@@ -20,7 +21,6 @@ def search_goods():
 @app.route('/showGoods')
 def show_goods():
     if hasattr(g, 'userid'):
-        print(g.userid)
         return render_template('showGoods.html')
     return redirect(url_for('login'))
 
@@ -28,8 +28,6 @@ def show_goods():
 @app.route('/createGoods')
 def create_goods():
     if hasattr(g, 'userid'):
-        print(g.role)
-        print(len(subscriber.block_chain.chain))
         if g.role == 'Meteria':
             productID = generateUUID()
             productID.strip()
@@ -37,7 +35,6 @@ def create_goods():
         else:
             chainName = isCanAddGoods(g.role)
             return render_template('createGoods.html', type='O', block=chainName, role=g.role)
-
 
         # if (len(subscriber.block_chain.chain)==0 and g.role=="Meteria") or (len(subscriber.block_chain.chain) == 1 and g.role == "Product") or (len(subscriber.block_chain.chain) == 2 and g.role == "Transport") or (len(subscriber.block_chain.chain) == 3 and g.role == "Sale"):
         #     return render_template('createGoods.html',role=g.role, disabled="false")
@@ -52,8 +49,8 @@ def modify_goods():
         mysql = MysqlService()
         userIn = mysql.getUserInforByID(g.userid)
 
-        chainName=isCanEditGoods(g.role)
-        return render_template('modifyGoods.html',type='M',block=chainName,userrole=g.role)
+        chainName = isCanEditGoods(g.role)
+        return render_template('modifyGoods.html', type='M', block=chainName, userrole=g.role)
     return redirect(url_for('login'))
 
 
@@ -62,7 +59,6 @@ def userInfor():
     if hasattr(g, 'userid'):
         mysql = MysqlService()
         userIn = mysql.getUserInforByID(g.userid)
-        print(userIn)
         return render_template('userInfor.html', userid=g.userid, username=userIn[0], userphone=userIn[1],
                                userrole=userIn[2])
     return redirect(url_for('login'))
@@ -70,18 +66,6 @@ def userInfor():
 
 @app.route('/user/getGoods', methods=['GET', 'POST'])
 def getGood():
-    # data = request.get_data()
-    # data = str(data, encoding="utf8")
-    # j_data = json.loads(data)
-    # if request.method=='POST':
-    #     id = j_data['id']
-    #     print(id)
-    # else:
-    #     print('get')
-
-    # get his role
-    # get info from the block by his role
-    # blockchain = Blockchain()
     res = subscriber.block_chain.full_chain()
     return res
 
@@ -99,11 +83,14 @@ def addGoods():
     discription = request.form['product_des']
     state = request.form['status']
     # add the goods to the blockchain
-    dict = {'product_id': product_id, 'name': product_name, 'address': address, 'date': data, 'description': discription,
+    dict = {'product_id': product_id, 'name': product_name, 'address': address, 'date': data,
+            'description': discription,
             'status': state}
     # block=Blockchain()
     mysql = MysqlService()
     mysql.addChainIdToUser(g.userid, product_id)
+    if subscriber.block_chain.valid_chain() is False:
+        subscriber.synchronizing()
     subscriber.send_message('new block', dict)
     while new_flag is False:
         pass
@@ -126,7 +113,7 @@ def editGoods():
     data = request.form['date']
     discription = request.form['product_des']
     state = request.form['status']
-    index=changeRoleToNum(state)
+    index = changeRoleToNum(state)
     # add the goods to the blockchain
     dict = {'index': index, 'number': product_id, 'name': product_name, 'address': address, 'date': data,
             'description': discription,
@@ -141,6 +128,7 @@ def editGoods():
     else:
         return render_template('modifyGoods.html', res='success')
 
+
 @app.route('/user/tourist', methods=['GET'])
 def enterTouristMode():
     chainName_m = isCanEditGoods('Meteria')
@@ -148,44 +136,45 @@ def enterTouristMode():
     chainName_t = isCanEditGoods('Transport')
     chainName_s = isCanEditGoods('Sale')
 
-    chainName=chainName_m+chainName_p+chainName_t+chainName_s
-    chainName_set=set(chainName)
-    chainName=list(chainName_set)
+    chainName = chainName_m + chainName_p + chainName_t + chainName_s
+    chainName_set = set(chainName)
+    chainName = list(chainName_set)
 
     return render_template('touristMode.html', type='O', block=chainName)
 
 
 def isCanAddGoods(currentRole):
-    mysql=MysqlService()
+    mysql = MysqlService()
 
-    currentNum=changeRoleToNum(currentRole)
+    currentNum = changeRoleToNum(currentRole)
 
-    canAddChainID=mysql.getCanAddChainID(currentNum-1)
-    print(canAddChainID)
+    canAddChainID = mysql.getCanAddChainID(currentNum - 1)
     return canAddChainID
+
 
 def isCanEditGoods(currentRole):
-    mysql=MysqlService()
+    mysql = MysqlService()
 
-    currentNum=changeRoleToNum(currentRole)
+    currentNum = changeRoleToNum(currentRole)
 
-    canAddChainID=mysql.getCanAddChainID(currentNum)
-    print(canAddChainID)
+    canAddChainID = mysql.getCanAddChainID(currentNum)
     return canAddChainID
 
+
 def changeRoleToNum(role):
-    if role=='Meteria':
+    if role == 'Meteria':
         return 1
-    if role=='Product':
+    if role == 'Product':
         return 2
-    if role=='Transport':
+    if role == 'Transport':
         return 3
-    if role=='Sale':
+    if role == 'Sale':
         return 4
 
+
 def generateUUID():
-    productid_uu=uuid.uuid1()
-    productid_uu_str=str(productid_uu)
-    productid=productid_uu_str[0:8]
-    print('Generate the product id: '+productid)
+    productid_uu = uuid.uuid1()
+    productid_uu_str = str(productid_uu)
+    productid = productid_uu_str[0:8]
+    print('Generate the product id: ' + productid)
     return productid
